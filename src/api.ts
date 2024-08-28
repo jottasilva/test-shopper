3bcea55b941022e68b8471b9e605aca64b76356b
unknown <jefferson_jsp@hotmail.com>
2024-08-27 22:01:10 -0300

    genini running

--- src/api.ts
+++ src/api.ts
@@ -1,18 +1,30 @@
 require('dotenv').config(); 
-const { GoogleGenerativeAI } = require("@google/generative-ai");
-
-const genAI = new GoogleGenerativeAI(process.env.API_KEY);
-const model = genAI.getGenerativeModel({ model: "genini-1.5-flash" });
+import {GoogleAIFileManager} from '@google/generative-ai/server';
+import { GoogleGenerativeAI } from '@google/generative-ai';
+const API_KEY = process.env.API_KEY;
+const genAI = new GoogleGenerativeAI(String(API_KEY));
+const model = genAI.getGenerativeModel({model: "gemini-1.5-pro"});
 // leitura de dados
-async function generateText(prompt: string) {
-    try {
-        const result = await model.generateContent(prompt);
-        const response = await result.response;
-        const text = await response.text(); 
-        console.log(text);
-    } catch (error) {
-        console.error("Erro ao tentar Ler Imagem:", error);
-    }
+async function generateText(img: string) {
+    const fileImg = new GoogleAIFileManager(String(API_KEY));
+    const uploadResponse = await fileImg.uploadFile("./uploads/Lista-compras.jpeg", {
+        mimeType: "image/jpeg",
+        displayName: "Lista de Compras"
+    });
+     console.log(uploadResponse.file.displayName);
+     console.log(uploadResponse.file.state)
+     console.log(uploadResponse.file.uri);
+
+     const result = await model.generateContent([
+        {
+          fileData: {
+            mimeType: uploadResponse.file.mimeType,
+            fileUri: uploadResponse.file.uri
+          }
+        },
+        { text: "Describe how this product might be manufactured." },
+      ]);
+      console.log(result.response.text());
 
 }
 export {generateText};

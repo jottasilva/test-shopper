3a8887734890e09941bcf73ac9b07957c34475ae
unknown <jefferson_jsp@hotmail.com>
2024-08-28 21:12:50 -0300

    genini running + base64 received

--- index.ts
+++ index.ts
@@ -1,23 +1,54 @@
-
-
-import { generateText } from "./src/api";
+import { base64image } from "./src/api";
 import app from "./src/server";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
+var isBase64 = require("is-base64");
 const PORT = parseInt(`${process.env.SV_PORT || 3000}`);
-const API_KEY = process.env.API_KEY;
 // Uplad Image
-app.get('/readimgxx',async (req, res) => {
-    await generateText("Quem descobriu o brasil, e como foi ?");
+app.post("/upload", async (req, res) => {
+  const { image, costumer_code, measure_datetime, measure_type } = req.body;
+
+  if (image && measure_type && measure_datetime && costumer_code) {
+    // Base64 Verification
+    if (isBase64(image, { mimeRequired: true })) {
+      try {
+        await base64image(image, measure_type);
+        res.status(200).json({
+          image_url: image,
+          measure_value: 123,
+          measure_uuid: 15,
+        });
+      } catch {
+        res.status(400).json({
+            error_code: "INVALID_DATA",
+            error_description:
+              "Ocorreu um erro ao tentar Acessar a API",
+          });
+      }
+    } else {
+      res.status(400).json({
+        error_code: "INVALID_DATA",
+        error_description:
+          "Os dados fornecidos no corpo da requisição são inválidos",
+      });
+    }
+  } else {
+    res.status(400).json({
+      error_code: "INVALID_DATA",
+      error_description:
+        "Os dados fornecidos no corpo da requisição são inválidos",
+    });
+  }
 });
 // Confirm Image
-app.patch('/confirmimg', (req, res) => {
-    res.send("Confirm Image :D");
+app.patch("/confirm", (req, res) => {
+  res.send("Confirm Image :D");
 });
 // Update Image
-app.get('/listreads', (req, res) => {
-    res.send("List all image Reads");
+app.get("/:costumer_code/list", (req, res) => {
+  const code = req.params.costumer_code;
+  res.send(code);
 });
-app.listen(PORT,()=> {
-    console.log("Server is Running, Port "+PORT);
-})
+app.listen(PORT, () => {
+  console.log("Server is Running, Port " + PORT);
+});
--- src/api.ts
+++ src/api.ts
@@ -1,30 +1,41 @@
-require('dotenv').config(); 
-import {GoogleAIFileManager} from '@google/generative-ai/server';
-import { GoogleGenerativeAI } from '@google/generative-ai';
+require("dotenv").config();
+import { GoogleAIFileManager } from "@google/generative-ai/server";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { writeFileSync } from "fs";
+import path from 'path';
+import { v4 as uuidv4 } from 'uuid';
+
 const API_KEY = process.env.API_KEY;
 const genAI = new GoogleGenerativeAI(String(API_KEY));
-const model = genAI.getGenerativeModel({model: "gemini-1.5-pro"});
-// leitura de dados
-async function generateText(img: string) {
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
+const UUID = uuidv4();
+// Data read
+async function base64image(img: string, type:string) {
+  const imgBuffer = Buffer.from(img, "base64");
+  const tempImg = path.join(__dirname,`/tmp_img_${UUID}.jpg`);
+  const nameImg = `Leitura - ${type} - ${UUID}.png`;
+  writeFileSync(tempImg, imgBuffer);
+  try {
     const fileImg = new GoogleAIFileManager(String(API_KEY));
-    const uploadResponse = await fileImg.uploadFile("./uploads/Lista-compras.jpeg", {
-        mimeType: "image/jpeg",
-        displayName: "Lista de Compras"
+    const uploadResponse = await fileImg.uploadFile(tempImg, {
+      mimeType: "image/jpeg",
+      displayName: nameImg,
     });
-     console.log(uploadResponse.file.displayName);
-     console.log(uploadResponse.file.state)
-     console.log(uploadResponse.file.uri);
-
-     const result = await model.generateContent([
-        {
-          fileData: {
-            mimeType: uploadResponse.file.mimeType,
-            fileUri: uploadResponse.file.uri
-          }
+    // Result generation API
+    console.log(img.toString);
+    const result = await model.generateContent([
+      {
+        fileData: {
+          mimeType: uploadResponse.file.mimeType,
+          fileUri: uploadResponse.file.uri,
         },
-        { text: "Describe how this product might be manufactured." },
-      ]);
-      console.log(result.response.text());
+      },
+      { text: "Leia esta Imagem e me de a numeração da Leitura" },
+    ]);
+    console.log(result.response.text());
+  } catch (error) {
 
+    throw new Error("Api Error : " + error);
+  }
 }
-export {generateText};
+export { base64image };
